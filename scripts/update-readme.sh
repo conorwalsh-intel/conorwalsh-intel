c84d158452826d89c30fac46d29e8c3d43d84391
Conor Walsh <conor.walsh@intel.com>
2021-09-14 18:03:10 +0100

    Update update-readme.sh

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -14,13 +14,14 @@ done
 
 cd ..
 
-awk -v data="$(<tmp.md)" '/START_SECTION:dpdk/ {f=1} /END_SECTION:dpdk/ && f {print data; f=0}1' README.md > README.md.tmp; mv README.md.tmp README.md
+awk -v data="$(<tmp.md)" '/START_SECTION:dpdk/ {f=1} /END_SECTION:dpdk/ && f {print data; f=0}1' ../README.md > ../README.md.tmp; mv ../README.md.tmp ../README.md
 
 touch tmpgen.md
 echo "_Generated: $(env TZ=Europe/Dublin date +%Y\-%m\-%d\ %l%P\ \I\S\T)_" > tmpgen.md
 
-awk -v data="$(<tmpgen.md)" '/START_SECTION:gen/ {f=1} /END_SECTION:gen/ && f {print data; f=0}1' README.md > README.md.tmp; mv README.md.tmp README.md
+awk -v data="$(<tmpgen.md)" '/START_SECTION:gen/ {f=1} /END_SECTION:gen/ && f {print data; f=0}1' ../README.md > ../README.md.tmp; mv ../README.md.tmp ../README.md
 
 rm -rf dpdk
 rm tmp.md
 rm tmpgen.md
+rm ../README.md.tmp
