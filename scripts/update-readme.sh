3f9aaa85b13205b8fc007d9564e0f25937bc59f8
Conor Walsh <conor.walsh@intel.com>
2021-09-14 17:57:44 +0100

    Update update-readme.sh

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -2,19 +2,25 @@
 
 git clone http://dpdk.org/git/dpdk
 
-sed -i '1,/Automated after this line/!d' README.md
-
-cd dpdk
+touch tmp.md
 
 IFS=$'\n'
 
+touch tmp.md
+
 for OUT in $(git log --oneline --author conor.walsh@intel.com -n 10); do
-        echo $OUT | cut -d' ' -f1 | { read id; echo "* [${OUT#* }](https://github.com/DPDK/dpdk/commit/$id)" >> ../README.md; }
+        echo $OUT | cut -d' ' -f1 | { read id; echo "* [${OUT#* }](https://github.com/DPDK/dpdk/commit/$id)" >> ../tmp.md; }
 done
 
 cd ..
 
-echo "" >> README.md
-echo "_Generated: $(env TZ=Europe/Dublin date +%Y\-%m\-%d\ %l%P\ \I\S\T)_" >> README.md
+awk -v data="$(<tmp.md)" '/START_SECTION:dpdk/ {f=1} /END_SECTION:dpdk/ && f {print data; f=0}1' README.md > README.md.tmp; mv README.md.tmp README.md
+
+touch tmpgen.md
+echo "_Generated: $(env TZ=Europe/Dublin date +%Y\-%m\-%d\ %l%P\ \I\S\T)_" > tmpgen.md
+
+awk -v data="$(<tmpgen.md)" '/START_SECTION:gen/ {f=1} /END_SECTION:gen/ && f {print data; f=0}1' README.md > README.md.tmp; mv README.md.tmp README.md
 
 rm -rf dpdk
+rm tmp.md
+rm tmpgen.md
