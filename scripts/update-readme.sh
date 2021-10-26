7ad9773c3d1cd9ba7e6aa6ed23dffd61bc8aab19
Conor Walsh <conor.walsh@intel.com>
2021-10-26 13:58:46 +0100

    Update update-readme.sh

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -9,7 +9,7 @@ cd dpdk
 IFS=$'\n'
 
 echo "<!--START_SECTION:dpdk-->" >> ../tmp.md
-for OUT in $(git log --oneline --author conor.walsh@intel.com -n 10); do
+for OUT in $(git log --oneline --author conor.walsh@intel.com -n 15); do
         echo $OUT | cut -d' ' -f1 | { read id; echo "* [${OUT#* }](https://github.com/DPDK/dpdk/commit/$id)" >> ../tmp.md; }
 done
 echo "<!--END_SECTION:dpdk-->" >> ../tmp.md
@@ -20,7 +20,7 @@ sed -e '/START_SECTION:dpdk/,/END_SECTION:dpdk/!b' -e '/END_SECTION:dpdk/!d;r tm
 
 touch tmpgen.md
 echo "<!--START_SECTION:gen-->" >> tmpgen.md
-echo "_Generated: $(env TZ=Europe/Dublin date +%Y\-%m\-%d\ %l%P\ \I\S\T)_" >> tmpgen.md
+echo "_Generated: $(env TZ=Europe/Dublin date +%Y\-%m\-%d\ %l%P\ \G\M\T)_" >> tmpgen.md
 echo "<!--END_SECTION:gen-->" >> tmpgen.md
 
 sed -e '/START_SECTION:gen/,/END_SECTION:gen/!b' -e '/END_SECTION:gen/!d;r tmpgen.md' -e 'd' README.md > README.md.tmp; mv README.md.tmp README.md
